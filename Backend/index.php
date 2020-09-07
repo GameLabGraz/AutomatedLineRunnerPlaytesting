a86564f1be399763ec871626db2ef621dd30102e
Michael Steurer <m.steurer@student.tugraz.at>
2020-09-07 10:54:18 +0200

    Added unity and backend project files.

---
+++ Backend/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Automatic Playtesting</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://github.com/twysvn/sbecky.js/releases/download/v1.0/sbecky.min.js" charset="utf-8"></script>
+    </head>
+    <body>
+
+        <h1>Bakk</h1>
+
+        <nav>
+            <ul>
+                <li><a href="http://twysvn.com/webgl">Generate</a></li>
+                <li><a href="http://twysvn.com/play">Play</a></li>
+                <li><a href="games.php">Games</a></li>
+                <li><a href="ratings.php">Ratings</a></li>
+                <li><a href="morecharts.php">Extra Charts</a></li>
+            </ul>
+        </nav>
+
+    </body>
+</html>
